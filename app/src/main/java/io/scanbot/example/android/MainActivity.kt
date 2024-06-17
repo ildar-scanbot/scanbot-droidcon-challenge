3c3b4f905f81f1d914e12782930a66c103e41365
Ildar Iskakov <ildar@scanbot.io>
2024-06-17 13:57:55 +0200

    Initial commit

---
+++ app/src/main/java/io/scanbot/example/android/MainActivity.kt
@@ -0,0 +1,23 @@
+package io.scanbot.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var resultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.scan_button).setOnClickListener {
+            // TODO: Implement call to RTU UI screen here
+
+        }
+        resultTextView = findViewById(R.id.result_text_view)
+
+        // This TextView will be used to display the result of the scan
+        resultTextView.text = "The result of the barcode scan will be displayed here"
+    }
+}
